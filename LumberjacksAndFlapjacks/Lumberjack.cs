b528929f22230d4ae4694206703ba06b6373e461
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 10:53:01 +0800

    Add project files.

---
+++ LumberjacksAndFlapjacks/Lumberjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LumberjacksAndFlapjacks
+{
+    class Lumberjack
+    {
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public string Name { get; private set; }
+
+        public Lumberjack(string name)
+        {
+            this.Name = name;
+        }
+
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            this.flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{this.Name} is eating flapjacks");
+            while (this.flapjackStack.Count > 0)
+            {
+                Console.WriteLine(
+                    $"{this.Name} ate a {this.flapjackStack.Pop().ToString().ToLower()} flapjack");
+            }
+        }
+    }
+
+}
